6f4320e597cb7a8d14a6e85e1779cb557506e27b
Choong Han Yi <hanyichoong@gmail.com>
2023-05-05 22:59:59 +0800

    Added new languages

--- MarkovNameGenerator.py
+++ MarkovNameGenerator.py
@@ -2,10 +2,10 @@
 https://www.jeffcarp.com/posts/2019/markov-chain-python/
 """
 import argparse
+import random
 from typing import Optional, TypedDict
-import numpy as np
-from collections import defaultdict
 from json import load
+import markovify
 
 
 class MarkovLanguage(TypedDict):
@@ -16,48 +16,17 @@ class MarkovLanguage(TypedDict):
 
 
 class MarkovNameGen:
-    def _walk_graph(self, graph, distance=5, start_node=None) -> list[str]:
-        """Returns a list of words from a randomly weighted walk."""
-        if distance <= 0:
-            return []
-
-        # If not given, pick a start node at random.
-        if not start_node:
-            start_node = self.rng.choice(list(graph.keys()))
-
-        weights = np.array(
-            list(self.markov_graph[start_node].values()), dtype=np.float64
-        )
-        # Normalize word counts to sum to 1.
-        weights /= weights.sum()
-
-        # Pick a destination using weighted distribution.
-        choices = list(self.markov_graph[start_node].keys())
-        chosen_word = self.rng.choice(choices, None, p=weights)
-
-        return [chosen_word] + self._walk_graph(
-            graph, distance=distance - 1, start_node=chosen_word
-        )
-
     def generate_graph(self, filename: str):
         # Read text from file and tokenize.
-        with open(filename) as fp:
+        with open(filename, "r") as fp:
             self.language: MarkovLanguage = load(fp)
-        tokenized_text = [letter for letter in self.language["text"]]
 
-        # Create graph.
-        self.markov_graph = defaultdict(lambda: defaultdict(int))
+        corpus = list()
+        for word in self.language["text"].lower().split(" "):
+            corpus.append([x for x in word])
 
-        last_letter = tokenized_text[0].lower()
-        for letter in tokenized_text[1:]:
-            if letter == " ":
-                continue
-            letter = letter.lower()
-            self.markov_graph[last_letter][letter] += 1
-            last_letter = letter
-
-    def convert_to_number(self, string: str) -> int:
-        return int.from_bytes(string.encode(), "little")
+        self.generator = markovify.Text(None, parsed_sentences=corpus,
+                                        state_size=3)
 
     def generate_word(self, seed: str = None):
         if "dictionary" in self.language.keys():
@@ -65,11 +34,12 @@ class MarkovNameGen:
                 return self.language["dictionary"][seed.lower()]
 
         if seed is not None:
-            self.rng = np.random.default_rng(self.convert_to_number(seed))
-        minChar = self.language["minChar"]
-        maxChar = self.language["maxChar"]
-        distance = int((maxChar - minChar) * self.rng.random() + minChar)
-        return "".join(self._walk_graph(self.markov_graph, distance=distance))
+            random.seed(seed)
+        word = None
+        while word is None:
+            word = self.generator.make_sentence()
+
+        return word.replace(" ", "")
 
 
 def aparse() -> argparse.ArgumentParser:
